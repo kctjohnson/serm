f2d6ba8364e17c2c9b2606b5bd96be550bdf85bf
Killean Johnson <killean@Killeans-MacBook-Pro.local>
2023-05-17 22:02:01 -0400

    Start, Stop, and Check if service running functionality

--- process.go
+++ process.go
@@ -5,8 +5,20 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"syscall"
 	"time"
 )
+
+type ProcessInfo struct {
+	PID     int
+	OutPath string
+	ErrPath string
+}
+
+type Process struct {
+	ProcInfo ProcessInfo
+	Service  Service
+}
 
 type ProcessManager struct {
 	Processes []Process
@@ -20,24 +32,20 @@ func NewProcessManager() *ProcessManager {
 	}
 }
 
-type Process struct {
-	Info   ProcessInfo
-	Config Service
-}
-
-type ProcessInfo struct {
-	PID     int
-	OutPath string
-	ErrPath string
-}
-
 func (pm *ProcessManager) OnStartup() error {
 	for _, serv := range pm.Config.Services {
-		if serv.OnStartup == true {
-			err := pm.startProcess(serv)
+		// Check to see if we're already running the service
+		running, err := pm.ServiceRunning(serv)
+		if err != nil {
+			return err
+		}
+
+		// Begin the start process for this service if it runs on startup
+		if serv.OnStartup == true && !running {
+			err := pm.StartProcess(serv)
 			if err != nil {
 				for retries := 0; err != nil && serv.Retry && retries < serv.RetryCount; retries++ {
-					err = pm.startProcess(serv)
+					err = pm.StartProcess(serv)
 				}
 			}
 
@@ -49,17 +57,116 @@ func (pm *ProcessManager) OnStartup() error {
 	return nil
 }
 
-func (pm *ProcessManager) startProcess(service Service) error {
+func (pm ProcessManager) ServiceRunning(service Service) (bool, error) {
+	// First check to see if the service is in a running process currently
+	var foundProc *Process
+	for _, proc := range pm.Processes {
+		if proc.Service.Name == service.Name {
+			foundProc = &proc
+			break
+		}
+	}
+
+	// Then check to see if the PID is running
+	if foundProc != nil {
+		if foundProc.ProcInfo.PID <= 0 {
+			return false, fmt.Errorf("invalid foundProc.ProcInfo.PID %v", foundProc.ProcInfo.PID)
+		}
+
+		proc, err := os.FindProcess(int(foundProc.ProcInfo.PID))
+		if err != nil {
+			return false, err
+		}
+
+		err = proc.Signal(syscall.Signal(0))
+		if err == nil {
+			return true, nil
+		}
+
+		if err.Error() == "os: process already finished" {
+			return false, nil
+		}
+
+		errno, ok := err.(syscall.Errno)
+		if !ok {
+			return false, err
+		}
+
+		switch errno {
+		case syscall.ESRCH:
+			return false, nil
+		case syscall.EPERM:
+			return true, nil
+		}
+
+		return false, err
+	} else {
+		// TODO: Reassess if we would ever want to look for detached and missing procs
+		// 				Mostly because that shoooouldn't ever really happen if we keep track
+		// 				of the processes with a sqlite DB?
+		// // Search for the process
+		// cmdStr := fmt.Sprintf("ps aux | grep \"%s\" | awk '{print $2}'", "./cmd/printer/printer -type alph")
+		// cmd := exec.Command("bash", "-c", cmdStr)
+		// outBytes, err := cmd.Output()
+		// if err != nil {
+		// 	return false, err
+		// }
+		//
+		// outStr := string(outBytes)
+		// pids := strings.Split(outStr, "\n")
+		// if len(pids) >= 4 {
+		// 	programPid := pids[len(pids)-2]
+		// 	pm.Processes = append(pm.Processes, Process{
+		// 		ProcInfo: ProcessInfo{
+		//
+		// 		},
+		// 		Service:  service,
+		// 	})
+		// }
+	}
+
+	return false, nil
+}
+
+func (pm *ProcessManager) StartProcess(service Service) error {
 	procInfo, err := spawnDetachedProcess(pm.Config.LogPath, service.Bin, service.Args, service.Name)
 	if err != nil {
 		return err
 	}
 
 	pm.Processes = append(pm.Processes, Process{
-		Info:   procInfo,
-		Config: service,
+		ProcInfo: procInfo,
+		Service:  service,
 	})
 
+	return nil
+}
+
+func (pm *ProcessManager) StopProcess(proc Process) error {
+	// Get the actual process
+	foundProc, err := os.FindProcess(proc.ProcInfo.PID)
+	if err != nil {
+		return err
+	}
+
+	// Kill the process
+	err = foundProc.Kill()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pm *ProcessManager) RestartProcess(proc Process) error {
+	err := pm.StopProcess(proc)
+	if err != nil {
+		return err
+	}
+	err = pm.StartProcess(proc.Service)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -85,22 +192,22 @@ func spawnDetachedProcess(logPath string, binPath string, args []string, process
 	sOut := outFile
 	sErr := errFile
 
-	fmt.Printf("Creating command for %s with args %v\n", binPath, args)
+	// fmt.Printf("Creating command for %s with args %v\n", binPath, args)
 	cmd := exec.Command(binPath, args...)
 	cmd.Stdout = sOut
 	cmd.Stderr = sErr
 	cmd.Dir = filepath.Dir(binPath)
 
-	fmt.Printf("Starting command\n")
+	// fmt.Printf("Starting command\n")
 	err = cmd.Start()
 	if err != nil {
 		return ProcessInfo{}, err
 	}
 
-	fmt.Printf("Command PID: %d\n", cmd.Process.Pid)
+	// fmt.Printf("Command PID: %d\n", cmd.Process.Pid)
 	pid := cmd.Process.Pid
 
-	fmt.Printf("Releasing command\n")
+	// fmt.Printf("Releasing command\n")
 	err = cmd.Process.Release()
 	if err != nil {
 		return ProcessInfo{}, err
